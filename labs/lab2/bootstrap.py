24fb61d3345d37c207b52d7376cd42145e48a470
itsdaveba <sam@example.com>
2019-02-09 14:09:26 +0000

    Bootstrapping

--- labs/lab2/bootstrap.py
+++ labs/lab2/bootstrap.py
@@ -5,12 +5,17 @@ import seaborn as sns
 import numpy as np
 
 
-def boostrap(sample, sample_size, iterations):
+def bootstrap(sample, sample_size, iterations, ci):
 	new_samples = np.empty([0, sample_size])
-	for i in range(iterations):
+	means = []
+	for j in range(iterations):
 		new_sample = np.random.choice(sample, sample_size)
 		new_samples = np.vstack((new_samples, new_sample))
+		mean = np.mean(new_sample)
+		means.append(mean)
 	data_mean = np.mean(new_samples)
+	lower = np.percentile(means, 50-ci/2.)
+	upper = np.percentile(means, 50+ci/2.)
 	return data_mean, lower, upper
 
 
@@ -20,24 +25,20 @@ if __name__ == "__main__":
 	data = df.values.T[1]
 	boots = []
 	for i in range(100, 100000, 1000):
-		boot = boostrap(data, data.shape[0], i)
+		boot = bootstrap(data, data.shape[0], i, 95)
 		boots.append([i, boot[0], "mean"])
 		boots.append([i, boot[1], "lower"])
 		boots.append([i, boot[2], "upper"])
+		print(i)
 
 	df_boot = pd.DataFrame(boots, columns=['Boostrap Iterations', 'Mean', "Value"])
 	sns_plot = sns.lmplot(df_boot.columns[0], df_boot.columns[1], data=df_boot, fit_reg=False, hue="Value")
 
-	sns_plot.axes[0, 0].set_ylim(0,)
+	sns_plot.axes[0, 0].set_ylim(0, )
 	sns_plot.axes[0, 0].set_xlim(0, 100000)
 
 	sns_plot.savefig("bootstrap_confidence.png", bbox_inches='tight')
 	sns_plot.savefig("bootstrap_confidence.pdf", bbox_inches='tight')
 
-
-	#print ("Mean: %f")%(np.mean(data))
-	#print ("Var: %f")%(np.var(data))
-	
-
-
-	
+# print ("Mean: %f")%(np.mean(data))
+# print ("Var: %f")%(np.var(data))
